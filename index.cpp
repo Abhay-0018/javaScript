c709486173c6e43e56ab2f3d4b23f1b1951d68eb
Abhay-0018 <abhaykaiwart49@gmail.com>
2025-04-01 16:01:22 +0000

    container commit

---
+++ index.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"This is the first cpp code in the github";
+    return 0;
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("This is the first js program to run in the github")
+let a = 10;
+let b = 20;
+console.log(a+b);
+console.log('This is the sum')
